ba1c64eb44cbfeb7f1658b6f7ae04dd7bc626d84
Hung Hoang <hoangmanhhung008009@gmail.com>
2024-08-11 23:51:57 +0700

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import PostsModule from './posts/posts.module';
+import * as Joi from '@hapi/joi';
+import { AuthenticationModule } from './authentication/authentication.module';
+import CategoriesModule from './categories/categories.module';
+import SeriesModule from './series/series.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const uri = configService.get('MONGO_URI');
+        return {
+          uri: uri,
+        };
+      },
+      inject: [ConfigService],
+    }),
+    PostsModule,
+    AuthenticationModule,
+    CategoriesModule,
+    SeriesModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.use(cookieParser());
+  const config = new DocumentBuilder()
+    .setTitle('NestJs example')
+    .setDescription('The NestJs API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
