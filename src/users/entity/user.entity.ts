4f6338a7d8d88ff43f60475fc7a3112f50ed9879
hunghoang <hoangmanhhung008009@gmail.com>
2024-11-05 18:05:18 +0700

    update ci_cd.yml

--- src/users/entity/user.entity.ts
+++ src/users/entity/user.entity.ts
@@ -1,9 +1,12 @@
-import { Entity } from 'typeorm';
+import { Column, Entity } from 'typeorm';
 import { ShareEntity } from '../../base.entity';
 
 @Entity('users')
 export class UserEntity extends ShareEntity {
-  email: string;
-  firstName: string;
+  @Column()
+  phone: string;
+  @Column()
   password: string;
+  @Column()
+  role: string;
 }
